495848eeac357b9c7a9bc3dca00deb7d6635e604
F0F8FF <qkrtmdgml112@naver.com>
2024-05-27 18:12:11 +0900

    레이아웃 변경

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Page() {
       <p className="lg:text-7xl text-6xl shrink-0 text-gray-900">
           <span className="font-light">{"I'm"}</span>
           <br />
-          <span className="font-medium">Full-stack Developer</span>
+          <span className="font-medium">Backend Developer</span>
           <br />
-          <span className="font-light">currently work at Seoul.</span>
+          <span className="font-light">currently working in Seoul.</span>
+          <br />
+          <span className="font-light text-5xl">Becoming a full stack developer.</span>
         </p>
       {/* <div className="my-8">
         <BlogPosts />

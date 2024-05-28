2a3cdb528f55a9afd3feb644ccba6e62d21fdda7
F0F8FF <qkrtmdgml112@naver.com>
2024-05-28 23:36:03 +0900

    no message

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <h1 className="mb-20 text-2xl font-semibold tracking-tighter">
         F0F8FF.log
       </h1>
-      <h2 className="lg:text-7xl text-6xl shrink-0 text-gray-900">
+      <p className="lg:text-7xl text-6xl shrink-0">
           <span className="font-light">{"I'm"}</span>
           <br />
           <span className="font-medium">Backend Developer</span>
@@ -15,7 +15,7 @@ export default function Page() {
           <span className="font-light">currently working in Seoul.</span>
           <br />
           <span className="font-light text-5xl">Becoming a full stack developer.</span>
-        </h2>
+        </p>
 
         <Computer />
 
